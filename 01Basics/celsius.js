045c68ed2300122548792d4d7af3611b5a4eaf76
Sudhir Kumar <sudhir14107@iiitd.ac.in>
2019-01-14 19:26:57 +0530

    First Program

---
+++ 01Basics/celsius.js
@@ -0,0 +1,13 @@
+var score = 100
+const bonus = 50
+
+var finalScore = score + bonus
+var complex = 4+5*4/2
+
+// console.log(finalScore)
+// console.log(complex)
+
+let tempInFahrenheit = 32
+
+let celsius = (tempInFahrenheit - 32)*(5/9)
+console.log(celsius)
