056bcb24bf6c64ef89c9931d30664d9ba8a4888b
Sudhir Kumar <sudhir14107@iiitd.ac.in>
2019-01-14 17:50:16 +0530

    variable and const

---
+++ 01Basics/variable.js
@@ -0,0 +1,17 @@
+let name = 'Jhon'
+let score = 20
+let bonus = 20.0 
+let totalScore = score + bonus
+console.log(totalScore)
+
+let firstname = 'Sudhir'
+let lastName = 'Kumar'
+console.log(firstname +' '+  lastName)
+
+const a = 20
+//a = 20 Not Allowed cont cant be changed
+console.log(a) 
+
+var b = 10
+var b = 20
+console.log(b) //var change allowed but not let
