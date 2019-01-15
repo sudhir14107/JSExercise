962b535f347d2967ba619b44c8863fb185fd59c2
Sudhir Kumar <sudhir14107@iiitd.ac.in>
2019-01-15 09:40:52 +0530

    Functions basics

---
+++ 01Intermediate/01function.js
@@ -0,0 +1,22 @@
+let sayHello = function(name){
+    console.log(`Greeting for ${name}`)
+    console.log(`Hello ${name}`)
+}
+
+sayHello('sudhir')
+
+let fullNameMaker = function(firstName, lastName){
+    console.log("Greetings for you")
+    console.log(`Hello ${firstName} ${lastName}`)
+}
+
+fullNameMaker("Sudhir", "Kumar")
+
+let myAdder = function(num1, num2){
+    let sum = num1 + num2
+    return sum
+}
+
+let result = myAdder(2,3)
+console.log(myAdder(2,3))
+console.log(result)
