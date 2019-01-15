d0d651994142c2dd6c1d34c9cc23e3d6f3f8ea53
Sudhir Kumar <sudhir14107@iiitd.ac.in>
2019-01-15 08:36:20 +0530

    Array and templates

---
+++ 02Basics/marvel.js
@@ -0,0 +1,9 @@
+const superHeroes = ['IranMan', 'SpiderMan', 'Captain America', 'Thor']
+console.log(superHeroes)
+console.log(superHeroes[0])
+//console.log(superHeroes[-1]) not work as python
+console.log(superHeroes[superHeroes.length-1])
+
+console.log('we have 3 Super heroes')
+
+console.log(`we have ${superHeroes.length} super heroes`)
