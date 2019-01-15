7ee3a7697e703995d667c74d5f978285a42dd6db
Sudhir Kumar <sudhir14107@iiitd.ac.in>
2019-01-15 09:00:35 +0530

    methods of aaaray

--- 02Basics/marvel.js
+++ 02Basics/marvel.js
@@ -7,3 +7,27 @@ console.log(superHeroes[superHeroes.length-1])
 console.log('we have 3 Super heroes')
 
 console.log(`we have ${superHeroes.length} super heroes`)
+
+const numbers = ['one', 'two', 'three', 'four', 'five']
+
+numbers[1] = 'Something' //allowed
+
+const a = 4
+// a = 5 not allowed
+console.log(a)
+
+console.log(numbers.shift())
+console.log(numbers)
+
+console.log(numbers.unshift('Something'));
+console.log(numbers)
+
+numbers.splice(1,2,'sudhir')
+console.log(numbers)
+
+numbers.splice(2,2,'kumar','kumar')
+console.log(numbers)
+
+
+
+
